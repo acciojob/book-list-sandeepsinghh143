4fec5748a8a72ee967e1a397d224ff9a5d8757b9
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-22 17:45:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click", function(event) {
+      event.preventDefault();
+      addBookToList();
+    });
 let tbody=document.getElementById("book-list");
-function addBook(){
+function addBookToList(){
 	let row=tbody.insertRow();
 	let td1=row.insertCell(0);
 	td1.innerText=document.getElementById("title").value;
@@ -9,12 +13,23 @@ function addBook(){
 	let td3=row.insertCell(2);
 	td3.innerText=document.getElementById("isbn").value;
 	let td4=row.insertCell(3);
-	td4.innerText="X";
-	td4.innerText.style.backgroundColor="red";
-
+	let newBtn = document.createElement('button');
+	newBtn.className="delete";
+    newBtn.innerText = 'X';
+	td4.append(newBtn);
 	tr.append(td1);
 	tr.append(td2);
 	tr.append(td3);
 	tr.append(td4);
 	tbody.append(tr);
+      document.getElementById("title").value = "";
+      document.getElementById("author").value = "";
+      document.getElementById("isbn").value = ""
+	var deleteButtons = document.getElementsByClassName("delete");
+      for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener("click", function() {
+          var row = this.parentNode.parentNode;
+          row.parentNode.removeChild(row);
+        });
+      }
 }
