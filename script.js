1a92ed6a2d1d0aaba7b2cf2a51e5d7ea37acd572
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-22 17:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let tbody=document.getElementById("book-list");
+function addBook(){
+	let row=tbody.insertRow();
+	let td1=row.insertCell(0);
+	td1.innerText=document.getElementById("title").value;
+	let td2=row.insertCell(1);
+	td2.innerText=document.getElementById("author").value;
+	let td3=row.insertCell(2);
+	td3.innerText=document.getElementById("isbn").value;
+	let td4=row.insertCell(3);
+	td4.innerText="X";
+	td4.innerText.style.backgroundColor="red";
+
+	tr.append(td1);
+	tr.append(td2);
+	tr.append(td3);
+	tr.append(td4);
+	tbody.append(tr);
+}
